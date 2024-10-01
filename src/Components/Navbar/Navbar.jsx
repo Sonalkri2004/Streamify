8a901ee83077bd8399512427fd35c9677539fe89
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-10-01 12:47:50 +0530

    api setup done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,13 +7,14 @@ import upload_icon from '../../assets/upload.png'
 import more_icon from '../../assets/more.png'
 import notification_icon from '../../assets/notification.png'
 import profile_icon from '../../assets/jack.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = ({setSidebar}) => {
   return (
     <nav className='flex-div'>
         <div className="nav-left flex-div">
             <img src={menu_icon} alt="" className="menu-icon" onClick={()=>setSidebar(prev=>prev===false?true:false)} />
-            <img src={logo} alt="" className="logo" />
+            <Link to='/'><img src={logo} alt="" className="logo" /></Link>
 
         </div>
 
--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,98 +1,153 @@
-import React from 'react'
-import './PlayVideo.css'
+import React, { useEffect, useState } from "react";
+import "./PlayVideo.css";
 
-import video1 from '../../assets/video.mp4'
-import like from '../../assets/like.png'
-import dislike from '../../assets/dislike.png'
-import share from '../../assets/share.png'
-import save from '../../assets/save.png'
-import jack from '../../assets/jack.png'
-import user_profile from '../../assets/user_profile.jpg'
+import video1 from "../../assets/video.mp4";
+import like from "../../assets/like.png";
+import dislike from "../../assets/dislike.png";
+import share from "../../assets/share.png";
+import save from "../../assets/save.png";
+import jack from "../../assets/jack.png";
+import user_profile from "../../assets/user_profile.jpg";
+import { API_KEY, value_convertor } from "../../data";
+import moment from 'moment'
+const PlayVideo = ({videoId}) => {
 
+  const [apiData,setApiData] =useState(null);
 
-const PlayVideo = () => {
+  const fetchVideoData =async ()=>{
+    // Fetching videos data
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]))
+  }
+  useEffect(()=>{
+    fetchVideoData();
+  },[])
+
   return (
     <div className="play-video">
-        <video src={video1} controls autoPlay muted></video>
-        <h3>Best youtube channel to learn web devlopment</h3>
-        <div className="play-video-info">
-            <p>1525 views &bull; 2 days ago</p>
-            <div>
-                <span><img src={like} alt=''/>125</span>
-                <span><img src={dislike} alt=''/>1</span>
-                <span><img src={share} alt=''/>Share</span>
-                <span><img src={save} alt=''/>Save</span>
-            </div>
+      {/* <video src={video1} controls autoPlay muted></video> */}
+      <iframe
+        
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
+      <div className="play-video-info">
+        <p>{apiData?value_convertor(apiData.statistics.viewCount):"16K"} Views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""} </p>
+        <div>
+          <span>
+            <img src={like} alt="" />
+            125
+          </span>
+          <span>
+            <img src={dislike} alt="" />1
+          </span>
+          <span>
+            <img src={share} alt="" />
+            Share
+          </span>
+          <span>
+            <img src={save} alt="" />
+            Save
+          </span>
         </div>
-        <hr/>
-        <div className="publisher">
-            <img src={jack} alt=''/>
-            <div>
-                <p>GretaStack</p>
-               <span>1m subscribers</span>
-            </div>
-            <button>Subscribe</button>
+      </div>
+      <hr />
+      <div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>GretaStack</p>
+          <span>1m subscribers</span>
         </div>
-        <div className="vid-description">
-            <p>Channel that makes learning easy</p>
-            <p>Subscribe GreatStack to watch more content.</p>
-            <hr/>
-            <h4>130 comments</h4>
-            <div className="comment">
-                <img src={user_profile} alt=''/>
-                <div>
-                    <h3>Jack Nicholson <span>1 day ago</span></h3>
-                
-                     <p>A global computer network providing a variety of information and interconnected networks using standardized communication protocols.</p>
-                      <div className="comment-action">
-                        <img src={like} alt=''/>
-                        <span>244</span>
-                        <img src={dislike} alt='' />
-                      </div>
-                </div>
-            </div>
-            <div className="comment">
-                <img src={user_profile} alt=''/>
-                <div>
-                    <h3>Jack Nicholson <span>1 day ago</span></h3>
-                
-                     <p>A global computer network providing a variety of information and interconnected networks using standardized communication protocols.</p>
-                      <div className="comment-action">
-                        <img src={like} alt=''/>
-                        <span>244</span>
-                        <img src={dislike} alt='' />
-                      </div>
-                </div>
-            </div>
-            <div className="comment">
-                <img src={user_profile} alt=''/>
-                <div>
-                    <h3>Jack Nicholson <span>1 day ago</span></h3>
-                
-                     <p>A global computer network providing a variety of information and interconnected networks using standardized communication protocols.</p>
-                      <div className="comment-action">
-                        <img src={like} alt=''/>
-                        <span>244</span>
-                        <img src={dislike} alt='' />
-                      </div>
-                </div>
-            </div>
-            <div className="comment">
-                <img src={user_profile} alt=''/>
-                <div>
-                    <h3>Jack Nicholson <span>1 day ago</span></h3>
-                
-                     <p>A global computer network providing a variety of information and interconnected networks using standardized communication protocols.</p>
-                      <div className="comment-action">
-                        <img src={like} alt=''/>
-                        <span>244</span>
-                        <img src={dislike} alt='' />
-                      </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+        <button>Subscribe</button>
+      </div>
+      <div className="vid-description">
+        <p>Channel that makes learning easy</p>
+        <p>Subscribe GreatStack to watch more content.</p>
+        <hr />
+        <h4>130 comments</h4>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
 
-export default PlayVideo
+            <p>
+              A global computer network providing a variety of information and
+              interconnected networks using standardized communication
+              protocols.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+
+            <p>
+              A global computer network providing a variety of information and
+              interconnected networks using standardized communication
+              protocols.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+
+            <p>
+              A global computer network providing a variety of information and
+              interconnected networks using standardized communication
+              protocols.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+
+            <p>
+              A global computer network providing a variety of information and
+              interconnected networks using standardized communication
+              protocols.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayVideo;
